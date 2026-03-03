8ca4738ac9e8a8a955ea00b8211f36cc51f448b1
Emiliano Lopez <emiliano.ll289@gmail.com>
2026-03-02 18:16:11 -0600

    feat: Add basic Library page with user info and a placeholder
    - LibraryPage reads session server-side with creacteClientSS and getUser
    - Dsiplays user email as secondary info below page title
    - Show "No documents yet" placeholder
    - Error state redirects user to log out and try again

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,10 +1,32 @@
+import { createClientSS } from "@/lib/supabase/server";
 
-function libraryPage() {
+async function LibraryPage() {
+  const supabase = await createClientSS();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    return (
+      <div>
+        <h1>There wass an error, please log out and try again</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Library Page</h1>
+      <h1>Your Library</h1>
+      <div className="info">
+        {user.email}
+      </div>
+      <div className="documents-grid">
+        <h2>You don't have documents yet</h2>
+      </div>
     </div>
   );
 }
 
-export default libraryPage;
+export default LibraryPage;
